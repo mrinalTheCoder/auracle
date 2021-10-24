f5b0de7e0899084d71bfac0f37e9b382e5563849
Mrinal Jain <2mrinaljain@gmail.com>
2021-10-24 22:06:03 +0530

    instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import Instructions from './instructions.js';
 import ShapeMatching from "./shape-matching.js";
 import ColorMatching from "./color-matching.js";
 import ColorPicking from "./color-picking.js";
@@ -11,12 +12,14 @@ function App() {
     <>
       <BrowserRouter>
         <Switch>
+          <Route path={"/instructions"} component={Instructions} />
           <Route path={"/color-picking"} component={ColorPicking} />
           <Route path={"/shape-picking"} component={ShapePicking} />
           <Route path={"/color-matching"} component={ColorMatching} />
           <Route path={"/shape-matching"} component={ShapeMatching} />
         </Switch>
       </BrowserRouter>
+      <a href='/instructions'>Instructions</a> <br />
       <a href='/color-picking'>Color Picking</a> <br />
       <a href='/shape-picking'>Shape Picking</a> <br />
       <a href='/color-matching'>Color Matching</a> <br />
---
+++ src/instructions.js
@@ -0,0 +1,14 @@
+function Instructions() {
+  return (
+    <div className="App" style={{position: "relative", paddingBottom: "56.25%", height: 0}}>
+      <iframe
+        title="instructions"
+        src="https://www.loom.com/embed/eaaae7a18f9a47caa10fdfbcbec766f0"
+        frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen
+        style={{position:"absolute", top: 0, left: 0, width: "100%", height: "100%"}}
+      />
+    </div>
+  );
+}
+
+export default Instructions;
